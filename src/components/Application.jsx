58263de81a694a6228b0ea60183b5b5d5961b3b4
Charles Belcher <chuckbelcher@PGNET.local>
2020-03-05 23:17:15 -0500

    Added context for user and posts

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,45 +1,17 @@
 import React, { Component } from 'react';
-import { firestore, auth, createUserProfileDocument } from '../firebase'
 
 import Posts from './Posts';
-import { collectIdsAndDocs } from '../utilities';
 import Authenitication from './Authentication';
 
 class Application extends Component {
-  state = {
-    posts: [],
-    user: null,
-  };
-
-  unsubscribeFromFirestore = null;
-  unsubscribeFromAuth = null;
-
-  componentDidMount = async () => {
-    this.unsubscribeFromFirestore = firestore.collection('posts').onSnapshot(snapshot => {
-      const posts = snapshot.docs.map(collectIdsAndDocs);
-      this.setState({ posts })
-    });
-
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      const user = await createUserProfileDocument(userAuth)
-      console.log(user);
-      this.setState({ user });
-    });
-  }
-
-  componentWillUnmount = () => {
-    this.unsubscribeFromFirestore();
-  }
-
-
+  
   render() {
-    const { posts, user } = this.state;
 
     return (
       <main className="Application">
         <h1>Think Piece</h1>
-        <Authenitication user={user}/>
-        <Posts posts={posts} />
+        <Authenitication />
+        <Posts />
       </main>
     );
   }
