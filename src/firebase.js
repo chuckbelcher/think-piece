70d7429b157f57bac6148d04b2ec882d638083f7
Charles Belcher <chuckbelcher@PGNET.local>
2020-03-22 21:35:48 -0400

    Subscribed to user document for updates

--- src/firebase.js
+++ src/firebase.js
@@ -1,62 +1,61 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
-    apiKey: "AIzaSyAyh7ayf0LAB1rkopjOQWHKeA3XBdGN_qA",
-    authDomain: "think-piece-live-b6c4d.firebaseapp.com",
-    databaseURL: "https://think-piece-live-b6c4d.firebaseio.com",
-    projectId: "think-piece-live-b6c4d",
-    storageBucket: "think-piece-live-b6c4d.appspot.com",
-    messagingSenderId: "956610260971",
-    appId: "1:956610260971:web:8139fcaff7e9b6ac8cb3ae",
-    measurementId: "G-B9XD8K6KTB"
-  };
+  apiKey: "AIzaSyAyh7ayf0LAB1rkopjOQWHKeA3XBdGN_qA",
+  authDomain: "think-piece-live-b6c4d.firebaseapp.com",
+  databaseURL: "https://think-piece-live-b6c4d.firebaseio.com",
+  projectId: "think-piece-live-b6c4d",
+  storageBucket: "think-piece-live-b6c4d.appspot.com",
+  messagingSenderId: "956610260971",
+  appId: "1:956610260971:web:8139fcaff7e9b6ac8cb3ae",
+  measurementId: "G-B9XD8K6KTB"
+};
 
-  // Initialize Firebase
-  firebase.initializeApp(config);
+// Initialize Firebase
+firebase.initializeApp(config);
 
-  window.firebase = firebase;
+window.firebase = firebase;
 
-  export const firestore = firebase.firestore();
-  export const auth = firebase.auth();
-  export const firebaseSignOut = () => auth.signOut();
-  export const provider = new firebase.auth.GoogleAuthProvider();
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
-  export const createUserProfileDocument = async (user, additionalData) => {
-    if (!user) return;
-    
-    const userRef = firestore.doc(`users/${user.uid}`);
-    const snapshot = await userRef.get()
+export const firestore = firebase.firestore();
+export const auth = firebase.auth();
+export const firebaseSignOut = () => auth.signOut();
+export const provider = new firebase.auth.GoogleAuthProvider();
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const createUserProfileDocument = async (user, additionalData) => {
+  if (!user) return;
 
-    if (!snapshot.exists) {
-      const { displayName, email, photoURL } = user
-      const createdAt = new Date();
+  const userRef = firestore.doc(`users/${user.uid}`);
+  const snapshot = await userRef.get();
 
-      try {
-        await userRef.set({
-          displayName,
-          email,
-          photoURL,
-          createdAt,
-          ...additionalData,
-        });
-      } catch (error) {
-        console.error('Error creating user profile document', error.message);
-      }
-    }
+  if (!snapshot.exists) {
+    const { displayName, email, photoURL } = user;
+    const createdAt = new Date();
 
-    return getUserDocument(user.uid);
-  }
-
-  export const getUserDocument = async (uid) => {
-    if (!uid) return null;
     try {
-      const userDocument = await firestore.collection('users').doc(uid).get();
-      return { uid, ...userDocument.data() }
+      await userRef.set({
+        displayName,
+        email,
+        photoURL,
+        createdAt,
+        ...additionalData
+      });
     } catch (error) {
-      console.error('Error retrieving user profile document', error.message);
+      console.error("Error creating user profile document", error.message);
     }
   }
 
-  export default firebase;
+  return getUserDocument(user.uid);
+};
+
+export const getUserDocument = async uid => {
+  if (!uid) return null;
+  try {
+    return firestore.collection("users").doc(uid);
+  } catch (error) {
+    console.error("Error retrieving user profile document", error.message);
+  }
+};
+
+export default firebase;
--- src/providers/UserProvider.jsx
+++ src/providers/UserProvider.jsx
@@ -1,40 +1,37 @@
-import React, { Component, createContext } from 'react';
-import { auth, createUserProfileDocument} from '../firebase';
-
+import React, { Component, createContext } from "react";
+import { auth, createUserProfileDocument } from "../firebase";
 
 export const UserContext = createContext({ user: null });
 
-
 class UserProvider extends Component {
+  state = {
+    user: null
+  };
 
-    state = {
-        user: null,
-      };
-    
-      unsubscribeFromAuth = null;
-    
-      componentDidMount = async () => {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-          const user = await createUserProfileDocument(userAuth)
-          console.log(user);
-          this.setState({ user });
+  unsubscribeFromAuth = null;
+
+  componentDidMount = async () => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapshot => {
+          this.setState({ user: { uid: snapshot.id, ...snapshot.data() } });
         });
       }
-    
-      componentWillUnmount = () => {
-        this.unsubscribeFromFirestore();
-      }
-    
-    
-    render() {
+      this.setState({ user: userAuth });
+    });
+  };
 
-        const { user } = this.state;
-        const { children } = this.props
-        
-        return (
-        <UserContext.Provider value={user}>{children}</UserContext.Provider>
-        )
-    }
+  componentWillUnmount = () => {
+    this.unsubscribeFromFirestore();
+  };
+
+  render() {
+    const { user } = this.state;
+    const { children } = this.props;
+
+    return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
+  }
 }
 
 export default UserProvider;
